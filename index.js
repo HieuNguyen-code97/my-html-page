b4fc800e9a4731f1d2dd3ae3785499c3b3f26f4d
HieuNguyen*code97 <ndhieu190997@gmail.com>
2023-08-12 20:20:19 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const contentLetterSrart_actived = "Hãy nói gì đó ở đây trước khi người ấy mở bức thư nhé." //Lời mở đầu cho bức thư
-const mainContentLetter = "Gửi lời nhắn nhủ đến người bạn bạn yêu thương." //Nội dung của bức thư
+const contentLetterSrart_actived = "Chúc mừng sinh nhật chị nha!^^" //Lời mở đầu cho bức thư
+const mainContentLetter = "Thương trẻ trâu, Thương trẻ trâu, Thương trẻ trâu..lêu lêu😂😂😂 Xinh đẹp, thành công nha!^^" //Nội dung của bức thư
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
